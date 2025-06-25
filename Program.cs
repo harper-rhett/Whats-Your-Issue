12aa6e071c908f1954e0f89c0b9ebcfd5d5c5378
Harper Rhett <harperrhett@gmail.com>
2025-06-25 13:56:55 -0500

    Clean up

--- Program.cs
+++ Program.cs
@@ -8,37 +8,45 @@ using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using QuestPDF.Markdown;
 
+// Initialize
 Console.WriteLine("Welcome to What's Your Issue!");
-
 GitHubClient client = new(new ProductHeaderValue("WhatsYourIssue"));
 
+// Set up personal access token
 string personalAccessToken = UserInterface.RequestPersonalAccessToken(client);
 client.Credentials = new(personalAccessToken);
 
+// Request repository owner
 string repositoryOwner = UserInterface.RequestRepositoriesOwner(client);
 
+// Issue request settings
 RepositoryIssueRequest issueRequest = new()
 {
-	State = ItemStateFilter.All
+	State = ItemStateFilter.Open,
 };
 
+// Loop for requesting data
 while (true)
 {
+	// Request repository name
 	string repositoryName = UserInterface.RequestRepositoryName(client, repositoryOwner);
 
+	// Fetch repository issues
 	Console.WriteLine("\nFetching repository issues:");
 	IReadOnlyList<Issue> issues = await client.Issue.GetAllForRepository(repositoryOwner, repositoryName, issueRequest);
 	List<Issue> sanitizedIssues = new();
 	foreach (Issue issue in issues)
 	{
-		if (issue.PullRequest != null || issue.State == ItemState.Closed) continue;
+		if (issue.PullRequest != null) continue;
 		Console.WriteLine($"- {issue.Title}");
 		sanitizedIssues.Add(issue);
 	}
 
+	// Initialize output path
 	string outputPath = $"{Directory.GetCurrentDirectory()}\\Output";
 	Directory.CreateDirectory(outputPath);
 
+	// Export markdown
 	Console.WriteLine("\nBeginning markdown export.");
 	string markdownText = await Helper.IssuesToMarkdown(client, repositoryOwner, repositoryName, sanitizedIssues);
 	string markdownFileName = $"{repositoryName}_Issues.md";
@@ -46,16 +54,16 @@ while (true)
 	File.WriteAllText(markdownFilePath, markdownText);
 	Console.WriteLine($"Exported markdown to {markdownFilePath}");
 
+	// Export PDF
 	Console.WriteLine("\nBeginning PDF export.");
-
 	QuestPDF.Settings.License = LicenseType.Community;
 	Document document = Helper.MarkdownToPDF(markdownText);
-
 	string pdfFileName = $"{repositoryName}_Issues.pdf";
 	string pdfFilePath = $"{outputPath}\\{pdfFileName}";
 	document.GeneratePdf(pdfFilePath);
 	Console.WriteLine($"Exported PDF to {pdfFilePath}");
 
+	// Open output folder
 	Console.WriteLine("\nOpen output folder?");
 	Console.Write("Y/N: ");
 	string openFolderInput = Console.ReadLine();
